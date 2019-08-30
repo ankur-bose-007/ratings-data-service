7edf50ee0b6e72de6ac44771496473c9a98d163f
Ankur Bose <Ankur.Bose@cognizant.com>
2019-08-30 15:59:31 +0530

    level 1

---
+++ src/main/java/com/cts/ratingsdataservice/resources/RatingsDataResource.java
@@ -0,0 +1,28 @@
+package com.cts.ratingsdataservice.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cts.ratingsdataservice.models.Rating;
+import com.cts.ratingsdataservice.models.UserRating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsDataResource {
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId){
+		return new Rating(movieId,4);
+	}
+	
+	@RequestMapping("users/{userId}")
+	public UserRating getUserRating(@PathVariable("userId") String userId){
+		List<Rating> ratings = Arrays.asList(new Rating("1234", 4), new Rating("5678", 5));
+		UserRating userRating=new UserRating();
+		userRating.setUserRating(ratings);
+		return userRating;
+	}
+}
